44d92ce33d7d485c520da8445afd59a11b56c81f
luis@velogig.com <luiscg@MacBook-Air.local>
2026-04-09 20:48:46 -0500

    fix: usar Jira REST API v2

--- api/jira/[...path].js
+++ api/jira/[...path].js
@@ -15,7 +15,7 @@ module.exports = async function handler(req, res) {
   const pathParts = req.query.path || [];
   const key = pathParts[0];
   const isTest = key === '_test';
-  const apiPath = isTest ? '/rest/api/3/myself' : `/rest/api/3/issue/${key}?fields=summary,status`;
+  const apiPath = isTest ? '/rest/api/2/myself' : `/rest/api/2/issue/${key}?fields=summary,status`;
 
   try {
     const response = await fetch(`${base}${apiPath}`, {
